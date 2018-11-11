ebb41f13195f7e7e05518b3c7e6bb936c91d17b5
virgem gatchalian <densetsu.ghem@gmail.com>
2018-11-11 10:42:34 +0800

    + If Notification Needed Pop-up Notif

---
+++ design/service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('push', function(event) {
+          event.waitUntil(
+         
+            self.registration.showNotification('Critical Alert', {
+              body: 'Alert Message',
+            })
+          );
+        });
